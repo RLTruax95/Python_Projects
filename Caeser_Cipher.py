31b37b87ca662641a460b78b479101169c7c17b6
Ryan Truax <truax53@live.com>
2025-04-09 17:33:26 -0400

    Added the features necessary to allow for encryption and decryption in the same file

--- Caeser_Cipher.py
+++ Caeser_Cipher.py
@@ -1,11 +1,22 @@
 def main():
-    user_entry, shift_val = get_user_input()
-    print(encrypt(user_entry.lower(), shift_val))
+    get_user_input()
 
 def get_user_input():
-    temp_str = input('What would you like to encrypt?')
-    temp_val = int(input('What value would you like to shift by?'))
-    return temp_str, temp_val
+    while True:
+        temp_str = input('Would you like to (e)ncrypt or (d)ecrypt?')
+        if temp_str.lower() != 'e' and temp_str.lower() != 'd':
+            print('Invalid input. Try again.')
+            continue
+        else:
+            if temp_str.lower() == 'e':
+                temp_str = input('What would you like to encrypt?: ')
+                temp_val = int(input('What value would you like to shift by?: '))
+                print(encrypt(temp_str, temp_val))
+                break
+            else:
+                temp_str = input('What would you like to decrypt?: ')
+                decrypt(temp_str)
+                break
 
 def encrypt(phrase: str, shift_val: int):
     encrypted_phrase = ''
@@ -18,6 +29,10 @@ def encrypt(phrase: str, shift_val: int):
         if temp > ord('z'): temp -= 26
         encrypted_phrase = encrypted_phrase + chr(temp)
     return encrypted_phrase
+
+def decrypt(phrase: str):
+    for shift_val in range(0,25):
+        print(f'key {shift_val}: {encrypt(phrase, shift_val)}')
 
 if __name__ == '__main__':
     main()
