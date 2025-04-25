67e6417da0e7eaaf1cf9e6001b4cb2ee4fa08be0
Ryan Truax <truax53@live.com>
2025-04-24 22:46:34 -0400

    Completed and commented Conways game project

--- Conways_Game.py
+++ Conways_Game.py
@@ -1,18 +1,19 @@
 import os
 import random
+from time import sleep
 
 def main():
+    array = fill_array()
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        array = fill_array()
-        print_array(array)
-        check_life(array)
-        input()
+        os.system('cls' if os.name == 'nt' else 'clear')       #clear the screen in between print statements
+        print_array(array)      #print the new data to the screen
+        array = check_life(array)      #check to see if the next data lives or dies
+        sleep(0.5)  #delay for half a second
 
 def fill_array():   #Function used to fill the array with random 1's and 0's
-    array = [0] * 10
+    array = [0] * 20
     for i in range(0,len(array)):
-        row = [0] * 30
+        row = [0] * 79
         for j in range(0,len(row)):
             row[j] = random.randint(0,1)
         array[i] = row
@@ -23,35 +24,43 @@ def print_array(array):     #Function used to print the data to the terminal
         row = ''
         for j in range(0,len(array[i])):
             if array[i][j] == 1:
-                row = row + 'O '
+                row = row + 'O'
             else:
-                row = row + '  '
+                row = row + ' '
         print(row)
 
-def check_life(arr):
-    array = []
+def check_life(arr):    #checks to see if the data in each cell is alive or dead based on neighboring cells
+    array = []      #empty array to store the new data
     for i in range(0,len(arr)):
         row = []
-        for j in range(0,len(arr[i])):
+        for j in range(0,len(arr[i])):      #iterate through the whole array and check neighbors for life
             count = 0
             try:
-                if j != 0 and arr[i][j-1] == 1:     #Left boundary check
+                if arr[i-1][j] == 1:    #Upper cell check
                     count += 1
-                if j+1 != len(arr[i]) and arr[i][j+1] == 1:   #Right boundary check
+                if arr[i-1][j+1] == 1:    #Upper-right cell check
                     count += 1
-                if i != len(arr) and arr[i+1][j] == 1:    #Lower boundary check
+                if arr[i][j+1] == 1:    #Right cell check
                     count += 1
-                if i != 0 and arr[i-1][j] == 1: #Upper boundary check
+                if arr[i+1][j+1] == 1:    #Lower-right cell check
+                    count += 1
+                if arr[i+1][j] == 1:    #Lower cell check
+                    count += 1
+                if arr[i+1][j-1] == 1:    #Lower-left cell check
+                    count += 1
+                if arr[i][j-1] == 1:    # Left cell check
+                    count += 1
+                if arr[i-1][j-1] == 1:    #Upper-left cell check
                     count += 1
             except IndexError:
                 pass
-            if arr[i][j] == 1:
-                if count > 1: row.append(1)
+            if arr[i][j] == 1:      #assign the cell value based on the neighbors
+                if count == 2 or count == 3 : row.append(1)
                 else: row.append(0)
             elif arr[i][j] == 0:
                 if count == 3: row.append(1)
                 else: row.append(0)
-        array.append(row)
+        array.append(row)       #add each row to the newly created array
     return array
 
 if __name__ == '__main__':
